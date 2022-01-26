fee5a70d0bfcdcaedb2dd2b8d1ae4ae43de6fa8e
Benjamin Camacho <jahbenjah@hotmail.com>
2022-01-26 11:41:19 -0600

    Agregar archivos de proyecto.

---
+++ GeneradorQRWeb/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using GeneradorQRWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Diagnostics;
+
+namespace GeneradorQRWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Qr(string texto)
+        {
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(texto, QRCodeGenerator.ECCLevel.Q);
+            PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+            byte[] qrCodeImage = qrCode.GetGraphic(20);
+            string model = Convert.ToBase64String(qrCodeImage);
+            return View("Qr", model);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GenerarCodigoQR/Program.cs
@@ -0,0 +1,11 @@
+﻿using QRCoder;
+using System.Drawing;
+
+QRCodeGenerator qrGenerator = new QRCodeGenerator();
+QRCodeData qrCodeData = qrGenerator.CreateQrCode("Benjami Camacho", QRCodeGenerator.ECCLevel.Q);
+PngByteQRCode qrCode = new PngByteQRCode(qrCodeData);
+byte[] qrCodeImage = qrCode.GetGraphic(20);
+using var stream = new MemoryStream(qrCodeImage);
+Bitmap bitmap = new Bitmap(stream);
+bitmap.Save("qr.png");
+Console.WriteLine("Se guardo la imagen");
